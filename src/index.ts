7cd96ec9ad4b41beb53521ed02a32f285620b914
Mihailob5017 <mihailob5017@gmail.com>
2023-01-19 23:33:22 +0200

    created CRUD with GraphQL and Mikro-Orm

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
+import 'reflect-metadata';
 import { MikroORM } from '@mikro-orm/core';
 import mikroOrmConfig from './mikro-orm.config';
-// import { Post } from './entities/Post';
 
 import express from 'express';
 import { buildSchema } from 'type-graphql';
 import { ApolloServer } from 'apollo-server-express';
 import { HelloResolver } from './resolvers/hello';
+import { PostResolver } from './resolvers/post';
 const main = async () => {
 	const orm = await MikroORM.init(mikroOrmConfig);
 	await orm.getMigrator().up();
@@ -13,9 +14,10 @@ const main = async () => {
 
 	const apolloServer = new ApolloServer({
 		schema: await buildSchema({
-			resolvers: [HelloResolver],
+			resolvers: [HelloResolver, PostResolver],
 			validate: false,
 		}),
+		context: () => ({ em: orm.em }),
 	});
 
 	await apolloServer.start();
