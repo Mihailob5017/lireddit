6836aa5c73c1404d622152c579df2dd846c67c03
Mihailob5017 <mihailob5017@gmail.com>
2023-01-19 16:04:52 +0200

    set up apollo graphql

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,29 @@
 import { MikroORM } from '@mikro-orm/core';
 import mikroOrmConfig from './mikro-orm.config';
-import { Post } from './entities/Post';
+// import { Post } from './entities/Post';
 
+import express from 'express';
+import { buildSchema } from 'type-graphql';
+import { ApolloServer } from 'apollo-server-express';
+import { HelloResolver } from './resolvers/hello';
 const main = async () => {
 	const orm = await MikroORM.init(mikroOrmConfig);
 	await orm.getMigrator().up();
-	// const post = orm.em.create(Post, {
-	// 	title: 'my first post',
-	// 	createdAt: '',
-	// 	updatedAt: '',
-	// });
-	// await orm.em.persistAndFlush(post);
+	const app = express();
 
-	// const posts = await orm.em.find(Post, {});
-	// console.log(posts);
+	const apolloServer = new ApolloServer({
+		schema: await buildSchema({
+			resolvers: [HelloResolver],
+			validate: false,
+		}),
+	});
+
+	await apolloServer.start();
+	apolloServer.applyMiddleware({ app });
+
+	app.listen(4000, () => {
+		console.log('listening on 4000');
+	});
 };
 
 main().catch((err) => {
